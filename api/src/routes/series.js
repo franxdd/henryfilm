d480fe67b37fba538771ba1a7fb7fe493cddf398
Alejandro <ale.970@hotmail.com>
2022-07-19 19:55:10 -0300

    commit de series

--- api/src/routes/series.js
+++ api/src/routes/series.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.log("entro")
-=======
 const { Router } = require("express");
 const route = Router();
 const { getSeriesInfo } = require("../servicies/series.js")
@@ -9,4 +6,3 @@ require("dotenv").config();
 route.get("/", getSeriesInfo);
 
 module.exports = route;
->>>>>>> dev-back
