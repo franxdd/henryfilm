e3ac54df519a01a0dcb0be0bd6c4bebc79e52a68
francisco sequeira <Francisco@DESKTOP-G7UNRA6>
2022-07-19 13:15:12 -0300

    Creacion de Redux para laburar :FS

---
+++ client/src/Redux/Reducer/Reducer.js
@@ -0,0 +1,14 @@
+import { DETAIL } from "../Actions/Actions.js";
+const initialState = {
+  allmovies: [],
+  detail: {},
+};
+const rootRouter = (state = initialState, action) => {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
