1ddf1dbb768c78f1fb79e3014c619c3c1c5c72a4
francisco sequeira <Francisco@DESKTOP-G7UNRA6>
2022-07-19 19:34:04 -0300

    reducer y action

--- client/src/Components/CardSeries/CardSeries.jsx
+++ client/src/Components/CardSeries/CardSeries.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-
+import { React, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllSeries } from "../../Redux/Actions/Actions";
 function CardSeries() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllSeries());
+  }, []);
+  let { allSeries } = useSelector((state) => state);
   return <div>CardSeries</div>;
 }
 
--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -1,1 +1,15 @@
 export const DETAIL = "DETAIL";
+export const GET_ALL_SERIES = "GET_ALL_SERIES";
+
+export const getAllSeries = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3001/")
+      .then((r) => r.json())
+      .then((series) => {
+        dispatch({
+          type: GET_ALL_SERIES,
+          payload: series,
+        });
+      });
+  };
+};
