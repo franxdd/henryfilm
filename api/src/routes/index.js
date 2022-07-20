8f37a7ca0e06195be831ffc0b45f1b801707ef85
Edgar Rossi <edgarlrossi@gmail.com>
2022-07-20 13:15:16 -0300

    fran mete pull

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,11 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const router = Router();
-const peliculas  = require("./peliculas")
-const series = require('./series')
-const usuarios = require('./usuarios')
-
+const peliculas = require("./peliculas");
+const series = require("./series");
+const usuarios = require("./usuarios");
 
 router.use("/peliculas", peliculas);
 router.use("/series", series);
 router.use("/usuarios", usuarios);
-
 
 module.exports = router;
