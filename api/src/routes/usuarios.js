7ad2e11a3f81c6f27189f31ba30986b7278a4dd3
Eifro <afrotricks19@gmail.com>
2022-07-20 17:52:51 -0500

    post usuarios

--- api/src/routes/usuarios.js
+++ api/src/routes/usuarios.js
@@ -3,6 +3,6 @@ const route = Router();
 const { postUser } = require("../servicies/usuarios.js");
 require("dotenv").config();
 
-// route.post("/", postUser);
+route.post("/create", postUser);
 
 module.exports = route;
--- api/src/servicies/usuarios.js
+++ api/src/servicies/usuarios.js
@@ -1,21 +1,22 @@
 const axios = require("axios");
 require("dotenv").config();
 const { API_KEY } = process.env;
+const { Usuarios } = require('../DB/db')
 
 const postUser = async (req, res) => {
-  
- try {
-    console.log('entro')
-
-
-
-
- } catch (error) {
-    
- }
+  try {
+    const { telefono, nombre, apellido, email, pass } = req.body
+    if (!telefono || !nombre || !apellido || !email || !pass) return res.status(404).send("Falta completar un dato..")
+    const response = await Usuarios.findOrCreate({
+        where: { email },
+        defaults: { telefono, nombre, apellido, email, pass }
+    })
+    res.status(200).send(`Se creo el usuario ${response[0].dataValues.nombre}`)
+  } catch (error) {
+    res.status(404).send(error)
+  }
 };
 
-
 module.exports = {
-    postUser
+  postUser,
 };
