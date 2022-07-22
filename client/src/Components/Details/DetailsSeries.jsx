a07a934d561bcbc3f0bef1ed3e17c8c0225ace50
francisco sequeira <Francisco@DESKTOP-G7UNRA6>
2022-07-21 21:33:22 -0300

    Detalles de Series, Router por fin arreglado:FS

--- client/src/Components/Details/DetailsSeries.jsx
+++ client/src/Components/Details/DetailsSeries.jsx
@@ -1,19 +1,40 @@
 import { React, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSeriesDetail } from "../../Redux/Actions/Actions";
-function DetailsSeries(props) {
-  console.log(props);
+import { useParams } from "react-router-dom";
+function DetailsSeries() {
+  let { id } = useParams();
   const dispatch = useDispatch();
-  let { seriesDetail } = useSelector((state) => state);
-  const { id } = props.match.params;
+  let seriesDetail = useSelector((state) => state.seriesDetail);
   useEffect(() => {
     dispatch(getSeriesDetail(id));
-  }, [dispatch, id]);
-  // console.log(seriesDetail);
+  }, []);
+  // console.log(Array.isArray(seriesDetail.episode_run_time));
 
   return (
     <div>
-      <div>{seriesDetail}</div>
+      <div>
+        <h1>{seriesDetail.name}</h1>
+      </div>
+      <div>{seriesDetail.vote_average}</div>
+      <div>{seriesDetail.overview}</div>
+      <div>
+        {seriesDetail.episode_run_time?.map((e) => {
+          return <div>{e}</div>;
+        })}
+      </div>
+
+      {seriesDetail.genres?.map((e) => {
+        return <div>{e.name}</div>;
+      })}
+
+      <div>
+        {seriesDetail.production_companies?.map((e) => {
+          return <div>{e.name}</div>;
+        })}
+      </div>
+      <div>{seriesDetail.number_of_episodes}</div>
+      <div>{seriesDetail.number_of_seasons}</div>
     </div>
   );
 }
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
 import { React, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import { getAllSeries, getAllMovies } from "../../Redux/Actions/Actions";
 
 function Home() {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getAllSeries());
     dispatch(getAllMovies());
