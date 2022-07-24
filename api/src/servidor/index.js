1848fb76511417dad06d7574b5018cd533749079
Alejandro <ale.970@hotmail.com>
2022-07-24 02:00:41 -0300

    Deploy back

--- api/src/servidor/index.js
+++ api/src/servidor/index.js
@@ -1,7 +1,7 @@
 const server =require("../../App.js")
 const {conn} = require("../DB/db.js")
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: true }).then(() => {
     server.listen(PORT, () => {
