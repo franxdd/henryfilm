640ebacf898d951f63153cacba4faf35178b3954
Alejandro <ale.970@hotmail.com>
2022-07-25 15:15:30 -0300

    Configuracion lista para deploy: AR

--- api/src/servidor/index.js
+++ api/src/servidor/index.js
@@ -1,10 +1,10 @@
 const server =require("../../App.js")
 const {conn} = require("../DB/db.js")
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: false }).then(() => {
-    server.listen(PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log('%s listening at 3001'); // eslint-disable-line no-console
     });
   });
--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,11 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/Store/Store.js";
+import dotenv from "dotenv";
 // const dotenv = require ("dotenv");
 
-// dotenv.config();
-// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
